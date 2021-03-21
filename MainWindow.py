35a4424bb6376972693bc22c883aebcd062ae8f8
annomad <25004472@qq.com>
2021-03-22 00:04:23 +0800

    刚刚搞明白@pyqtSlot装饰器作用效果，原理还没太明白。

--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,4 @@
-from  PyQt5.QtWidgets import QMainWindow,QApplication
+from  PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
 from  PyQt5.uic import loadUi
 
 
@@ -12,9 +12,24 @@ class MainWindow(QMainWindow):
 
 
 
-# if __name__ == '__main__':
-#     import sys
-#     app = QApplication(sys.argv)
-#     ui = MainWindow()
-#     ui.show()
-#     sys.exit(app.exec())
+if __name__ == '__main__':
+    import sys
+    """这是用loadUi导入实例方式"""
+    testapp = QApplication(sys.argv)
+    testui = QMainWindow()      #ui是mainwindow还是widget类型很重要，上一步的实例化千万要弄清楚，搞错就报错！
+    loadUi('MainWindow_Ui.ui', testui) #经过测试，能用子类Qmainwindow，不用父类Qwidget
+
+
+    testui.show()
+    sys.exit(testapp.exec())
+
+
+
+
+
+
+"""这是传统的方式"""
+    # app = QApplication(sys.argv)
+    # ui = MainWindow()
+    # ui.show()
+    # sys.exit(app.exec())
--- slot_func.py
+++ slot_func.py
@@ -1,19 +1,26 @@
 """这是一个槽函数集中营，继承了QT designer设计的ui，加入诸多的槽函数"""
 from MainWindow import MainWindow
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import *
+from  PyQt5.uic import loadUi
 
 class Slotfunc(MainWindow):  #继承 QT designer设计界面的类
     def __init__(self):
         super().__init__()
-        self.openResource.clicked.connect(self.test)
-        self.a = 1
+        self.setWindowTitle('招投标制作工具')
+        # self.openResource.clicked.connect(self.test)
+        # self.a = 1
 
-    def test(self):
-        print("资料库按钮被点击类")
+
 
     @pyqtSlot()
-    def on_openResource_clicked(self):   #这是添加打开资料库的功能。
+    def on_openResource_clicked(self):   #这是添加打开资料库的功能。打开查询窗口
         print('这是个按钮和槽函数----自动绑定的测试:')
+        self.opendir_win = QDialog()
+        loadUi('open_dir.ui', self.opendir_win)
+        self.opendir_win.setWindowTitle('这是第二个窗口')
+        print(self.opendir_win.windowTitle())
+        self.opendir_win.show()
 
 
 
