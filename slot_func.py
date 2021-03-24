edc7512e65655c5aa06b38bf001493f44bcd1254
annomad <25004472@qq.com>
2021-03-25 00:38:42 +0800

    把窗口程序全部放在了MainWindow类去

--- slot_func.py
+++ slot_func.py
@@ -8,13 +8,12 @@ class Slotfunc(MainWindow):  #继承主窗口的类
     def __init__(self):
         super().__init__()
 
-
-
+    # 自动绑定信号和槽函数，点击"打开资料库"打开目录选择窗口
     @pyqtSlot()
     def on_openResource_clicked(self):
         self.opendirwin = OpenDirWin()
         self.opendirwin.show()
-        self.opendirwin.setWindowFlags()
+        # self.opendirwin.setWindowFlags()# 打开窗口，并设置了独占窗口
 
     # @pyqtSlot(
     # def on_openResourceWinOk_clicked(self):    #查询窗口的确定按钮
