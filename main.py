043f92ceff624b87506c1807c6fe17e042aa8556
annomad <25004472@qq.com>
2021-03-27 23:12:29 +0800

    拟正在编写检索功能

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from PyQt5.QtCore import pyqtSlot,QDir,QFile,QIODevice,QTextStream
 
 if __name__ == '__main__':
 
-
  app = QApplication(sys.argv) #定义个程序app，用于申请硬件系统资源
+
 
  #思路直接用loadUi形成窗口类MainWindow（），在用slotfunc（）继承MainWindow（）类，并改写。
  # 这样的好处就是把槽函数和Ui函数惯例。
