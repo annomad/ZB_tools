f710b97551d1c8d322fc0f1d393b1d646e482bc0
annomad <25004472@qq.com>
2021-04-03 20:16:38 +0800

    拟正在编写检索功能

--- main.py
+++ main.py
@@ -6,13 +6,9 @@ from PyQt5.QtCore import pyqtSlot,QDir,QFile,QIODevice,QTextStream
 if __name__ == '__main__':
 
  app = QApplication(sys.argv) #定义个程序app，用于申请硬件系统资源
-
-
  #思路直接用loadUi形成窗口类MainWindow（），在用slotfunc（）继承MainWindow（）类，并改写。
  # 这样的好处就是把槽函数和Ui函数惯例。
  from  slot_func import  Slotfunc
  mywindow = Slotfunc() #以按键为线索的类给他实例化
-
-
  mywindow.show()
  sys.exit(app.exec())   #惯例写法，让程序循环。
--- slot_func.py
+++ slot_func.py
@@ -15,31 +15,28 @@ class Slotfunc(MainWindow):  #继承主窗口的类
         #fileName1, filetype = QFileDialog.getOpenFileName(self, "选取文件", "./","All Files (*);;Excel Files (*.xls)")  # 设置文件扩展名过滤,注意用双分号间隔
 
         self.dir_path = QFileDialog.getExistingDirectory(self, "选取文件夹", "./") #打开目录
+        print('xxxx')
         print(self.dir_path)
+        self.dir_model = QFileSystemModel()  # 实例化一个QfilesystemModel
+        self.dispay_dir_path.setText(self.dir_path)  # 路径显示label控件显示路径的名称
+        self.dispay_dir_path.setToolTip(self.dir_path)
 
         # 判断程序是否有非none返回。
         if self.dir_path:
-            self.dispay_dir_path.setText(self.dir_path) #路径显示label控件显示路径的名称
-            self.dir_model = QFileSystemModel()  #实例化一个QfilesystemModel
-
             # # 进行筛选只显示文件夹，不显示文件和特色文件
             #dir_model.setFilter(QtCore.QDir.Dirs | QtCore.QDir.NoDotAndDotDot)
-
-
             self.dir_model.setRootPath(self.dir_path) #设置根目录
 
-            self.dir_treeView.setModel(self.dir_model)  #把设置好的目录model传递给treeview
-            self.dir_treeView.setRootIndex(self.dir_model.index(self.dir_path))# 把目录索引传递给treeview索引。
+        self.dir_treeView.setModel(self.dir_model)  # 把设置好的目录model传递给treeview
+        self.dir_treeView.setRootIndex(self.dir_model.index(self.dir_path))  # 把目录索引传递给treeview索引。
+
 
     @pyqtSlot()  # 装饰函数
     def on_search_button_clicked(self):
-        print(self.search_lineedit.text())
+        if self.search_lineedit.text() != None:
+            print(self.search_lineedit.text())
+            # self.dir_model.setNameFilters()
 
-        self.dir_model.setRootPath(self.dir_path)  # 设置根目录
-        self.dir_model.setNameFilters(self.search_lineedit.text())
-
-        self.dir_treeView.setModel(dir_model)  # 把设置好的目录model传递给treeview
-        self.dir_treeView.setRootIndex(dir_model.index(self.dir_path))  # 把目录索引传递给treeview索引
 
     # def search(str):    #定义一个信号
     #         str = self.search_linedit.Text()
